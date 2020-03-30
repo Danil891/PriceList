dbe02fc7cd8eb8c489b417d8ee1b03f54fdfccfe
Danil <cherepkov.dd@edu.spbstu.ru>
2020-03-30 20:09:07 +0300

    С предыдущим проектом были проблемы, пришлось пересоздать его, сюда залил уже готовую версию. Тесты

--- src/test/java/price/ListTest.java
+++ src/test/java/price/ListTest.java
@@ -23,13 +23,42 @@ public class ListTest{
 
     @Test
     public void testRemoveProduct() {
+        List priceList1 = new List();
+        priceList1.addProduct(2, new List.Product("milk", 7, 0));
+        priceList1.addProduct(1, new List.Product("mars", 1, 50));
+
+        List priceList2 = new List();
+        priceList2.addProduct(2, new List.Product("milk", 7, 0));
+        priceList2.addProduct(1, new List.Product("mars", 1, 50));
+        priceList2.addProduct(3, new List.Product("bread" , 5, 0  ));
+        assertNotEquals(priceList1, priceList2);
+        priceList2.removeProduct(3);
+        assertEquals(priceList1, priceList2);
     }
 
     @Test
     public void testChangeProduct() {
+        List priceList1 = new List();
+        priceList1.addProduct(2, new List.Product("milk", 7, 0));
+        priceList1.addProduct(1, new List.Product("mars", 1, 50));
+
+        List priceList2 = new List();
+        priceList2.addProduct(2, new List.Product("milk", 7, 0));
+        priceList2.addProduct(1, new List.Product("mars", 1, 50));
+        priceList2.changeProduct(1, new List.Product("bread" , 5, 0  ));
+
+        assertNotEquals(priceList1, priceList2);
     }
 
     @Test
     public void testCountPrice() {
+        List priceList1 = new List();
+        priceList1.addProduct(1, new List.Product("mars", 1, 50));
+        priceList1.addProduct(2, new List.Product("milk", 7, 0));
+
+        assertEquals(priceList1.countPrice(1, 3), 4, 50);
+        assertEquals(priceList1.countPrice(2, 4), 28, 0);
+
+
     }
 }
